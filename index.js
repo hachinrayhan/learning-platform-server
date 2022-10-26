fa4118839eb5832efa63631a6673c5505a72def7
hachinrayhan <rayhan.hachin@gmail.com>
2022-10-26 22:01:43 +0600

    course details added

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const courses = require('./data/courses.json');
+
 app.get('/', (req, res) => {
     res.send('API is running');
 });
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
 
 app.listen(port, () => {
     console.log('Server is running on port', port);
